9824ad30c30a5ae48c4ae4a35bba4c0e034197cd
홍지윤 <h_zhirun@naver.com>
2024-10-25 03:42:17 +0900

    feat: 아이돌 리스트 API 함수 작성

---
+++ src/apis/getIdolList.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://fandom-k-api.vercel.app/11-1';
+
+export const getIdolList = async ({ offset = 0, limit = 50 }) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/idols?offset=${offset}&limit=${limit}`
+    );
+
+    if (!response.ok) {
+      throw new Error('아이돌 데이터를 가져오는 데 실패했어요.');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('아이돌 데이터를 불러오는 중 오류가 발생했어요:', error);
+    throw error;
+  }
+};
