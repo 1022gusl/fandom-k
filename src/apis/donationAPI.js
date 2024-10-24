197b0d1ab0aa15b4f4e0cd30c120124036469257
홍지윤 <h_zhirun@naver.com>
2024-10-24 22:06:02 +0900

    feat: 후원 금액 조회 API 함수 작성

--- src/apis/donationAPI.js
+++ src/apis/donationAPI.js
@@ -1,23 +1,23 @@
 const BASE_URL = 'https://fandom-k-api.vercel.app/11-1';
+const headers = {
+  'Content-Type': 'application/json',
+};
 
+// 도네이션 추가 및 수정 API
 async function putDonations(donationId, amount) {
   const donationURL = `${BASE_URL}/donations/${donationId}/contribute`;
 
   try {
     let response = await fetch(donationURL, {
       method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers,
       body: JSON.stringify({ amount }),
     });
 
     if (response.status === 404) {
       response = await fetch(donationURL, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers,
         body: JSON.stringify({ amount }),
       });
     }
@@ -30,4 +30,23 @@ async function putDonations(donationId, amount) {
   }
 }
 
-export default putDonations;
+// 도네이션 조회 API
+async function getDonations(donationId) {
+  const donationURL = `${BASE_URL}/donations/${donationId}/contribute`;
+
+  try {
+    const response = await fetch(donationURL);
+
+    if (!response.ok) {
+      throw new Error('후원 데이터를 가져오는 데 실패했어요.');
+    }
+
+    const donationData = await response.json();
+    return donationData;
+  } catch (error) {
+    console.error('후원 데이터를 가져오는 중 오류가 발생했어요:', error);
+    throw error;
+  }
+}
+
+export { putDonations, getDonations };
