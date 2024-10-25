6d6cf971477ccef583d0b1b1008426fb6ccfe26e
홍지윤 <h_zhirun@naver.com>
2024-10-25 22:44:20 +0900

    feat: 공통 컴포넌트 Header 구현

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css';
+import logo from '../../assets/images/logo.png';
+import ProfileIcon from '../../assets/icons/profileIcon.svg';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles['header-contents']}>
+        <img src={logo} className={styles.logo} alt="팬덤케이 로고" />
+        <img src={ProfileIcon} className={styles.profile} alt="프로필" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
