f909afe0be215872c0a69ff7a9c27929588e41dd
unknown <smm45108@gmail.com>
2024-10-26 16:05:37 +0900

    refactor: Button 컴포넌트 스타일 객체로 분리하여 가독성 개선

--- src/pages/List/ChartOfTheMonth/components/common/Button.jsx
+++ src/pages/List/ChartOfTheMonth/components/common/Button.jsx
@@ -6,24 +6,23 @@ const Button = ({
   color = "#fff",
   background = "linear-gradient(90deg, #f86f65 0%, #fe5493 100%)",
   border = "none",
-  borderRadius = "3px", // 기본 borderRadius 설정
+  borderRadius = "3px",
   onClick,
   children,
   className,
 }) => {
+  // 버튼 스타일 객체 생성
+  const buttonStyle = {
+    height,
+    width,
+    color,
+    background,
+    border,
+    borderRadius,
+  };
+
   return (
-    <button
-      onClick={onClick}
-      style={{
-        height,
-        width,
-        color,
-        background,
-        border,
-        borderRadius,
-      }}
-      className={className}
-    >
+    <button onClick={onClick} style={buttonStyle} className={className}>
       {children}
     </button>
   );
