291733cd66170cfb8dbb45fc056ed0f4c02cf8b0
twtwkim <alfog23@gmail.com>
2024-10-27 00:51:39 +0900

    maintain fullidollist although page change

--- src/components/specific/AddFavoriteIdol.jsx
+++ src/components/specific/AddFavoriteIdol.jsx
@@ -38,10 +38,8 @@ const AddFavoriteIdol = () => {
       const pageSize = 100;
       try {
         setLoading(true);
-        // cursor가 없을 때 API 호출
-        const result = await getIdolList(
-          cursor ? { cursor, pageSize } : { pageSize }
-        );
+        // 전체 아이돌 목록 가져오기
+        const result = await getIdolList({ pageSize });
         setFullIdolList(result.list); // 전체 목록 상태에 저장
       } catch (err) {
         setError(err.message);
@@ -51,9 +49,7 @@ const AddFavoriteIdol = () => {
     };
 
     fetchAllIdols(); // 컴포넌트 마운트 시 전체 아이돌 목록 가져오기
-  }, [cursor]);
-
-  console.log(fullIdolList);
+  }, []);
 
   useEffect(() => {
     // 로컬 스토리지에서 선택된 아이돌을 가져와 상태 업데이트
@@ -65,8 +61,6 @@ const AddFavoriteIdol = () => {
     if (nextCursor) {
       setPrevCursor((prev) => [...prev, cursor]); // 현재 cursor를 이전 cursor 스택에 추가
       setCursor(nextCursor); // 다음 cursor로 이동
-      // 새로운 페이지를 로드할 때 favoriteIdols가 포함되도록 idolList를 다시 fetch합니다.
-      fetchIdols(nextCursor); // 다음 페이지의 아이돌 리스트를 가져옴
     }
   };
 
@@ -74,13 +68,10 @@ const AddFavoriteIdol = () => {
     if (prevCursor.length > 0) {
       setCursor(prevCursor[prevCursor.length - 1]); // 스택의 마지막 값을 cursor로 설정
       setPrevCursor((prev) => prev.slice(0, prev.length - 1)); // 스택에서 마지막 값을 제거
-      // 이전 페이지를 로드할 때 favoriteIdols가 포함되도록 idolList를 다시 fetch합니다.
-      fetchIdols(prevCursor[prevCursor.length - 1]); // 이전 페이지의 아이돌 리스트를 가져옴
     }
   };
 
   const handleSelectIdol = (idol) => {
-    // 선택 토글
     setSelectedIdols((prev) => {
       if (prev.includes(idol.id)) {
         return prev.filter((id) => id !== idol.id); // 이미 선택된 경우 선택 해제
@@ -88,20 +79,15 @@ const AddFavoriteIdol = () => {
         return [...prev, idol.id]; // 선택되지 않은 경우 선택
       }
     });
-    console.log(selectedIdols);
   };
 
   const handleSaveToLocalStorage = () => {
-    // 기존의 favoriteIdols를 가져옴
     const storedIdols = JSON.parse(localStorage.getItem("selectedIdols")) || [];
-    // 기존 아이돌과 새로운 선택된 아이돌 결합
     const updatedFavoriteIdols = [
       ...new Set([...storedIdols, ...selectedIdols]),
     ];
-    // 업데이트된 favoriteIdols를 로컬 스토리지에 저장
     localStorage.setItem("selectedIdols", JSON.stringify(updatedFavoriteIdols));
     alert("선택한 아이돌이 로컬 스토리지에 저장되었습니다!");
-    // 상태 업데이트
     setFavoriteIdols(updatedFavoriteIdols);
     setSelectedIdols([]); // 선택된 아이돌 초기화
   };
@@ -117,7 +103,6 @@ const AddFavoriteIdol = () => {
 
   if (error) return <p>Error: {error}</p>;
 
-  console.log(favoriteIdols);
   return (
     <>
       <div className="favoriteContainer">
