3e160b1178aa939c52ba1e192415728ec93cdab4
unknown <smm45108@gmail.com>
2024-10-26 17:39:04 +0900

    refactor: 색상 코드 SCSS 변수명으로 변경하여 관리 개선

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Button = ({
   height = "40px",
   width = "234px",
-  color = "#fff",
-  background = "linear-gradient(90deg, #f86f65 0%, #fe5493 100%)",
+  color = "$color-white-1",
+  background = "linear-gradient(90deg, #f86f65 0%, $color-brand-pink 100%)",
   onClick,
   children,
   className, // className을 props로 받아서
