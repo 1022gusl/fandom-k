06c51797241768c3675ee5ddab1f8920eb825204
unknown <smm45108@gmail.com>
2024-10-26 19:25:17 +0900

    refactor : 충돌 방지를 위한 리팩토링

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ChartPage from "./pages/List/ChartOfTheMonth/ChartPage";
+import "./styles/global.scss";
+function App() {
+  return (
+    <div>
+      <ChartPage />
+    </div>
+  );
+}
+export default App;
