ac10926acd0980358f52be6f6e6f01bae0257747
OhJinHwa <80371898+0hjinhwa@users.noreply.github.com>
2024-10-28 19:45:30 +0900

    img import 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import ChartPage from "./pages/List/ChartOfTheMonth/ChartPage";
+//import ChartPage from "./pages/List/ChartOfTheMonth/ChartPage";
+import LandingPage from "./pages/List/ChartOfTheMonth/LandingPage.jsx";
 import "./styles/global.scss";
 
 function App() {
   return (
     <div>
-      <ChartPage />
+      <LandingPage />
     </div>
   );
 }
--- src/pages/List/ChartOfTheMonth/LandingPage.jsx
+++ src/pages/List/ChartOfTheMonth/LandingPage.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './LandingPage.scss';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
-import from ''../../../assets/images/.png';
+import web01 from '../../../assets/images/web_01.png';
+import fandomKLogo from '../../../assets/images/Fandom-K.png';
+import main from '../../../assets/images/main.png';
+import home1 from '../../../assets/images/Home-1.png';
+import home2 from '../../../assets/images/Home-2.png';
+import home3 from '../../../assets/images/Home-3.png';
+import web02 from '../../../assets/images/web_02.png';
+import web03 from '../../../assets/images/web_03.png';
 
 function LandingPage() {
   return (
     <div className="all">
       <div className="container1">
         <div className="image-box">
-          <img src=".png" className="idol" alt="idol background" />
+          <img src={main} className="idol" alt="idol background" />
         </div>
 
         <div className="text-box1">
         <h2>내가 좋아하는 아이돌을 가장<span className="highlight"> 쉽게 덕질 </span>하는 방법
           </h2>
-          <img src="/Fandom-K.png" className="logo" alt="Fandom-K logo" />
+          <img src={fandomKLogo} className="logo" alt="Fandom-K logo" />
           <div className="start-text">지금 시작하기</div>
         </div>
       </div>
@@ -33,11 +33,11 @@ function LandingPage() {
           </div>
 
           <div className="linear-gradient">
-            <img src="/back2.png" className="radial" alt="background gradient" />
+            <img src={web01} className="radial" alt="background gradient" />
           </div>
 
           <div className="background-mask">
-            <img src="/Home-1.png" className="phone-image" alt="phone screen 1" />
+            <img src={home1} className="phone-image" alt="phone screen 1" />
           </div>
         </div>
       </div>
@@ -50,11 +50,11 @@ function LandingPage() {
           </div>
 
           <div className="background-gradi-except">
-            <img src="/web_02.png" className="ra" alt="background gradient" />
+            <img src={web02} className="ra" alt="background gradient" />
           </div>
 
           <div className="web02">
-            <img src="/Home-2.png" className="phone-image" alt="phone screen 2" />
+            <img src={home2} className="phone-image" alt="phone screen 2" />
           </div>
         </div>
       </div>
@@ -68,11 +68,11 @@ function LandingPage() {
 
           <div className="linear-gradient">
             <img 
-              src="/web_03.png" className="radial" alt="background gradient" />
+              src={web03} className="radial" alt="background gradient" />
           </div>
 
           <div className="web03">
-            <img src="/Home-3.png" className="phone-image" alt="phone screen 3" />
+            <img src={home3} className="phone-image" alt="phone screen 3" />
           </div>
         </div>
       </div>
