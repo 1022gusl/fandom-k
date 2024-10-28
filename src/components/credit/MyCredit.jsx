2398af9c7d8f5566d79704a274aa7ad3088dd02a
홍지윤 <h_zhirun@naver.com>
2024-10-28 18:55:26 +0900

    feat: MyCredit 컴포넌트 스타일 구현

--- src/components/credit/MyCredit.jsx
+++ src/components/credit/MyCredit.jsx
@@ -1,17 +1,18 @@
+import styles from '../credit/MyCredit.module.scss';
 import CreditIcon from '../../assets/icons/credit.svg';
 
 const MyCredit = () => {
   return (
-    <section className="creditWrapper">
-      <div className="creditBox">
-        <div className="creditInfo">
-          <h2 className="creditTitle">내 크레딧</h2>
-          <div className="creditAmount">
-            <img src={CreditIcon} alt="크레딧" className="creditIcon" />
+    <section className={styles.creditWrapper}>
+      <div className={styles.creditBox}>
+        <div className={styles.creditInfo}>
+          <h2 className={styles.creditTitle}>내 크레딧</h2>
+          <div className={styles.creditAmount}>
+            <img src={CreditIcon} alt="크레딧" className={styles.creditIcon} />
             36,000 {/* 스타일 확인차 임의 값 넣어두었습니다. */}
           </div>
         </div>
-        <div className="rechargeButton">충전하기</div>
+        <div className={styles.rechargeButton}>충전하기</div>
       </div>
     </section>
   );
