709f9db09fe40bbc529ede708ba8df3df06b5697
홍지윤 <h_zhirun@naver.com>
2024-10-29 17:11:23 +0900

    style: module.scss를 scss로 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,13 @@
-import styles from './Header.module.scss';
-import logo from '../../assets/images/logo.png';
-import ProfileIcon from '../../assets/icons/profileIcon.svg';
+import "./Header.scss";
+import logo from "../../assets/images/logo.png";
+import ProfileIcon from "../../assets/icons/profileIcon.svg";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles['header-contents']}>
-        <img src={logo} className={styles.logo} alt="팬덤케이 로고" />
-        <img src={ProfileIcon} className={styles.profile} alt="프로필" />
+    <header className="header">
+      <div className="header-contents">
+        <img src={logo} className="logo" alt="팬덤케이 로고" />
+        <img src={ProfileIcon} className="profile" alt="프로필" />
       </div>
     </header>
   );
--- src/components/credit/MyCredit.jsx
+++ src/components/credit/MyCredit.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import CreditModal from "../modals/CreditModal";
-import styles from "../credit/MyCredit.module.scss";
+import "../credit/MyCredit.scss";
 import CreditIcon from "../../assets/icons/credit.svg";
 
 const MyCredit = () => {
@@ -23,16 +23,16 @@ const MyCredit = () => {
   const closeModal = () => setIsModalOpen(false);
 
   return (
-    <section className={styles.creditWrapper}>
-      <div className={styles.creditBox}>
-        <div className={styles.creditInfo}>
-          <h2 className={styles.creditTitle}>내 크레딧</h2>
-          <div className={styles.creditAmount}>
-            <img src={CreditIcon} alt="크레딧" className={styles.creditIcon} />
+    <section className="creditWrapper">
+      <div className="creditBox">
+        <div className="creditInfo">
+          <h2 className="creditTitle">내 크레딧</h2>
+          <div className="creditAmount">
+            <img src={CreditIcon} alt="크레딧" className="creditIcon" />
             {totalCredits}
           </div>
         </div>
-        <button className={styles.rechargeButton} onClick={openModal}>
+        <button className="rechargeButton" onClick={openModal}>
           충전하기
         </button>
       </div>
