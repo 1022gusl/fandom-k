b72e61ef6026bd2b8c0f207651b802b2640ba32a
김예도 <kyd629@naver.com>
2024-10-30 12:51:10 +0900

    style: 경고 모달창 스타일 수정

--- src/components/modals/AlertModal.jsx
+++ src/components/modals/AlertModal.jsx
@@ -8,7 +8,7 @@ const AlertModal = ({ isOpen, onClose }) => {
     <CommonModal isOpen={isOpen} onClose={onClose}>
       <div className="alertModal">
         <img src={CreditIcon} alt="크레딧 아이콘" className="creditIcon" />
-        <div>
+        <div className="alertMsg">
           앗! 투표하기 위한 <span>크레딧</span>이 부족해요
         </div>
         <Button width="100%" onClick={onClose}>
