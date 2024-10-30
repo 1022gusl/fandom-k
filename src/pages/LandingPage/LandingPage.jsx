afdf7c08314c4dd408412955394875dd5002ca0c
OhJinHwa <80371898+0hjinhwa@users.noreply.github.com>
2024-10-30 20:43:06 +0900

    네이밍 수정, scss로 파일변경

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import styles from "./LandingPage.module.css";
-import web01 from "../../assets/images/web_01.png";
+import "./LandingPage.scss";
 import fandomKLogo from "../../assets/images/Fandom-K.png";
 import main from "../../assets/images/main.png";
 import home1 from "../../assets/images/Home-1.png";
 import home2 from "../../assets/images/Home-2.png";
 import home3 from "../../assets/images/Home-3.png";
-import web02 from "../../assets/images/web_02.png";
-import web03 from "../../assets/images/web_03.png";
+import web1 from "../../assets/images/web_01.png";
+import web2 from "../../assets/images/web_02.png";
+import web3 from "../../assets/images/web_03.png";
 import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
@@ -19,102 +19,102 @@ function LandingPage() {
   };
 
   return (
-    <div className={styles.all}>
-      <div className={styles.container1}>
-        <div className={styles.imageBox}>
-          <img src={main} className={styles.idol} alt="idol background" />
+    <div className="all">
+      <section className="heroSection">
+        <div className="imageBox">
+          <img src={main} className="idol" alt="idol background" />
         </div>
 
-        <div className={styles.mainTitle}>
-          <h2 className={styles.heading}>
+        <div className="mainTitle">
+          <h2 className="heading">
             내가 좋아하는 아이돌을
             <br />
-            가장 <span className={styles.highlight}>쉽게 덕질 </span>하는 방법
+            가장 <span className="highlight">쉽게 덕질 </span>하는 방법
           </h2>
           <img
             src={fandomKLogo}
-            className={styles.logo}
+            className="logo"
             alt="Fandom-K logo"
             onClick={moveToList}
           />
-          <button className={styles.startText} onClick={moveToList}>
+          <button className="startText" onClick={moveToList}>
             지금 시작하기
           </button>
         </div>
-      </div>
+      </section>
 
-      <div className={styles.container2}>
-        <div className={styles.background}>
-          <div className={styles.donateIdol}>
-            <span className={styles.nolang}>후원하기</span>
-            <h2 className={styles.heading2}>
+      <section className="donateIntro">
+        <div className="background">
+          <div className="donateIntroText">
+            <span className="nolang">후원하기</span>
+            <h2 className="heading2">
               좋아하는 아이돌에게
               <br />
               쉽게 조공해 보세요
             </h2>
           </div>
 
-          <div className={styles.linearGradient}>
-            <img src={web01} className={styles.ra} alt="background gradient" />
+          <div className="linearGradient">
+            <img src={web1} className="ra" alt="background gradient" />
           </div>
 
-          <div className={styles.backgroundMask}>
+          <div className="backgroundMask">
             <img
               src={home1}
-              className={styles.phoneImage}
+              className="phoneImage"
               alt="phone screen 1"
             />
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className={styles.container3}>
-        <div className={styles.background}>
-          <div className={styles.followIdol}>
-            <span className={styles.nolang}>이달의 아티스트</span>
-            <h2 className={styles.heading2}>
+      <section className="monthlyArtistIntro">
+        <div className="background">
+          <div className="voteIntroText">
+            <span className="nolang">이달의 아티스트</span>
+            <h2 className="heading2">
               내 아티스트에게 1등의 <br />
               영예를 선물하세요
             </h2>
           </div>
 
-          <div className={styles.backgroundGradiExcept}>
-            <img src={web02} className={styles.ra} alt="background gradient" />
+          <div className="backgroundGradiExcept">
+            <img src={web2} className="ra" alt="gradient except" />
           </div>
 
-          <div className={styles.web02}>
+          <div className="home2">
             <img
               src={home2}
-              className={styles.phoneImage}
+              className="phoneImage"
               alt="phone screen 2"
             />
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className={styles.container4}>
-        <div className={styles.background}>
-          <div className={styles.voteIdol}>
-            <span className={styles.nolang}>나만의 아티스트</span>
-            <h2 className={styles.heading2}>
+      <section className="artistNewsIntro">
+        <div className="background">
+          <div className="newsIntroText">
+            <span className="nolang">나만의 아티스트</span>
+            <h2 className="heading2">
               좋아하는 아티스트들의 <br />
               소식을 모아보세요
             </h2>
           </div>
 
-          <div className={styles.linearGradient}>
-            <img src={web03} className={styles.ra} alt="background gradient" />
+          <div className="linearGradient">
+            <img src={web3} className="ra" alt="background gradient" />
           </div>
 
-          <div className={styles.web03}>
+          <div className="home3">
             <img
               src={home3}
-              className={styles.phoneImage}
+              className="phoneImage"
               alt="phone screen 3"
             />
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
