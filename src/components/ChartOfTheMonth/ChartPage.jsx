7f7ec30f44ff88c46e914de2138ebdccebaf4672
unknown <smm45108@gmail.com>
2024-10-30 23:18:54 +0900

    refactor: listPage 반응형 조정

--- src/components/ChartOfTheMonth/ChartPage.jsx
+++ src/components/ChartOfTheMonth/ChartPage.jsx
@@ -74,7 +74,7 @@ const ChartPage = () => {
   };
 
   return (
-    <div className="chartContainer">
+    <section className="chartContainer">
       <div className="chartHeader">
         <h2 className="chartName">이달의 차트</h2>
         <GradientButton
@@ -104,7 +104,7 @@ const ChartPage = () => {
         <LoadMoreButton onClick={handleLoadMore} />
       )}
       {isMoreLoading && <LoadingSpinner />}
-    </div>
+    </section>
   );
 };
 
