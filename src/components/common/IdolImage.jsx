66e095264c60dd0aa6bd5969ce8878cf48ab8553
1022gusl <1022gusl@gmail.com>
2024-11-01 16:16:53 +0900

    refactor: 투표아이돌 컴포넌트의 체크아이콘과 오버레이를 재사용 가능하게 아이돌이미지 컴포넌트로 이동

--- src/components/common/IdolImage.jsx
+++ src/components/common/IdolImage.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./IdolImage.scss";
+import checkIcon from "../../assets/icons/check.svg";
 
-const IdolImage = ({ src, alt = "IdolImage", size = "70px" }) => {
+const IdolImage = ({
+  src,
+  alt = "IdolImage",
+  size = "70px",
+  isActive,
+  checkIconSize = "30px",
+}) => {
   return (
-    <div className="imgBorder" style={{ "--size": size }}>
+    <div
+      className="imgBorder"
+      style={{ "--size": size, "--check-icon-size": checkIconSize }}
+    >
       <img src={src} alt={alt} className="idolImg" />
+      {isActive && (
+        <div className="idolOverlay">
+          <img src={checkIcon} alt="체크 표시" className="checkedIcon" />
+        </div>
+      )}
     </div>
   );
 };
