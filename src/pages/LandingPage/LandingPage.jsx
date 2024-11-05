6d6dbc1a611a59903352c65f3cb9922e74bc6935
OhJinHwa <80371898+0hjinhwa@users.noreply.github.com>
2024-11-05 15:17:02 +0900

    refactor: features data, constants 폴더로 분리

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./LandingPage.scss";
 import fandomKLogo from "../../assets/images/Fandom-K.svg";
-import home1 from "../../assets/images/Home-1.png";
-import home2 from "../../assets/images/Home-2.png";
-import home3 from "../../assets/images/Home-3.png";
-import web1 from "../../assets/images/web_01.png";
-import web2 from "../../assets/images/web_02.png";
-import web3 from "../../assets/images/web_03.png";
 import { useNavigate } from "react-router-dom";
 import { useCredit } from "../../hooks/useCredit";
 import HeroSection from './HeroSection.jsx';
 import FeatureSection from './FeatureSection.jsx';
-import "../../styles/global.scss";
+import { features } from '../../constants/features.js';
 
 function LandingPage() {
   const [showIntro, setShowIntro] = useState(true); // 인트로 상태
@@ -68,31 +62,7 @@ function LandingPage() {
     );
   }
 
-  const features = [
-    {
-      className: "donateIntro",
-      title: "후원하기",
-      description: "좋아하는 아이돌에게<br>쉽게 조공해 보세요",
-      backgroundImage: web1,
-      phoneImage: home1,
-      hasNavyStripe: true
-    },
-    {
-      className: "monthlyArtistIntro",
-      title: "이달의 아티스트",
-      description: "내 아티스트에게 1등의<br>영예를 선물하세요",
-      backgroundImage: web2,
-      phoneImage: home2
-    },
-    {
-      className: "artistNewsIntro",
-      title: "나만의 아티스트",
-      description: "좋아하는 아티스트들의<br>소식을 모아보세요",
-      backgroundImage: web3,
-      phoneImage: home3
-    }
-  ];
-
+  
   return (
     <div className="all">
       <HeroSection onStartClick={moveToList} />
